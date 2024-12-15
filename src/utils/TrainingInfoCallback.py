c8f1cbeb9caa8c54fe9a7b68019d4b1a3d517c90
Emilien <emilien.komlenovic@gmail.com>
2024-12-16 00:05:54 +0100

    Try to do news metrics but not well implemented

--- src/utils/TrainingInfoCallback.py
+++ src/utils/TrainingInfoCallback.py
@@ -2,80 +2,45 @@ import numpy as np
 from stable_baselines3.common.callbacks import BaseCallback
 
 class TrainingInfoCallback(BaseCallback):
-    def __init__(self, verbose=0):
-        super().__init__(verbose)
+    def __init__(self):
+        super().__init__()
         self.training_metrics = {
-            'timesteps': [],
-            'episode_rewards': [],
-            'episode_lengths': [],
-            'mean_rewards': [],
-            'mean_lengths': [],
-            'terminated_count': 0,
-            'truncated_count': 0,
-            'total_episodes': 0
+            "episode_rewards": [],  
+            "episode_lengths": [], 
         }
-        
+
         self.current_episode_reward = 0
         self.current_episode_length = 0
         self.custom_metrics = {}
 
     def _on_step(self) -> bool:
-        for reward, done, truncated in zip(
-            self.locals['rewards'], 
-            self.locals['dones'], 
-            self.locals.get('truncateds', [False] * len(self.locals['rewards']))
-        ):
-            self.current_episode_reward += reward
-            self.current_episode_length += 1
-            
-            # Vérifier si l'épisode est terminé ou tronqué
-            if done or truncated:
-                self.training_metrics['total_episodes'] += 1
-                
-                if done:
-                    self.training_metrics['terminated_count'] += 1
-                
-                if truncated:
-                    self.training_metrics['truncated_count'] += 1
-                
-                # Stocker les métriques de l'épisode
-                self.training_metrics['episode_rewards'].append(self.current_episode_reward)
-                self.training_metrics['episode_lengths'].append(self.current_episode_length)
-                
-                # Calculer et stocker les moyennes glissantes
-                if len(self.training_metrics['episode_rewards']) > 10:
-                    mean_reward = np.mean(self.training_metrics['episode_rewards'][-10:])
-                    mean_length = np.mean(self.training_metrics['episode_lengths'][-10:])
-                    
-                    self.training_metrics['mean_rewards'].append(mean_reward)
-                    self.training_metrics['mean_lengths'].append(mean_length)
-                
-                # Réinitialiser pour le prochain épisode
-                self.current_episode_reward = 0
-                self.current_episode_length = 0
-        
-        # Stocker le nombre de timesteps
-        self.training_metrics['timesteps'].append(self.num_timesteps)
-        
+        """Méthode appelée à chaque étape de l'entraînement."""
+        self.current_episode_reward += self.locals["rewards"][0]
+        self.current_episode_length += 1
+
+        if self.locals["dones"][0]:
+            self.training_metrics["episode_rewards"].append(self.current_episode_reward)
+            self.training_metrics["episode_lengths"].append(self.current_episode_length)
+
+            self.current_episode_reward = 0
+            self.current_episode_length = 0
+
         return True
 
     def _on_training_end(self) -> None:
-        # Calculer le taux de succès à l'entraînement
-        try:
-            train_success_rate = (self.training_metrics['terminated_count'] / 
-                                  self.training_metrics['total_episodes']) * 100
-        except ZeroDivisionError:
-            train_success_rate = 0
+        """Méthode appelée à la fin de l'entraînement."""
+        rewards = self.training_metrics["episode_rewards"]
+        lengths = self.training_metrics["episode_lengths"]
 
-        # Préparer les métriques finales
-        self.results = {
-            "train_success_rate": train_success_rate,
-            "train_episodes": self.training_metrics['total_episodes'],
-            "custom_metrics": self.custom_metrics
+        self.custom_metrics = {
+            "mean_reward": np.mean(rewards),
+            "std_reward": np.std(rewards),
+            "mean_length": np.mean(lengths)
         }
 
     def get_metrics(self):
         """
-        Méthode pour récupérer les métriques dans le format spécifié
+        Méthode pour récupérer les métriques calculées.
+        :return: Dictionnaire des métriques
         """
-        return self.results if hasattr(self, 'results') else {}
+        return self.custom_metrics
