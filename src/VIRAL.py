8a5e1fe7737893ac041110ce3791c2ea98b6a247
Cuzin-Rambaud <valentincuzinmurena.io>
2024-12-17 22:09:47 +0100

    end of multi-process

--- src/VIRAL.py
+++ src/VIRAL.py
@@ -2,7 +2,9 @@ import random
 import signal
 import sys
 from multiprocessing import Process, Queue
+from queue import Empty
 from logging import getLogger
+from time import sleep
 from typing import Callable, Dict, List
 from venv import logger
 
@@ -19,11 +21,12 @@ from stable_baselines3.common.env_util import make_vec_env
 import gymnasium as gym
 import torch
 
+
 class VIRAL:
     def __init__(
         self,
         learning_algo: Algo,
-        env_type : Environments,
+        env_type: Environments,
         objectives_metrics: List[callable] = [],
         model: str = "qwen2.5-coder",
         options: dict = {},
@@ -34,7 +37,7 @@ class VIRAL:
                 model (str): Language model for reward generation
                 learning_method (str): Reinforcement learning method
         """
-        if (options.get("seed") is None):
+        if options.get("seed") is None:
             options["seed"] = random.randint(0, 1000000)
 
         self.llm = OllamaChat(
@@ -51,17 +54,25 @@ class VIRAL:
         """,
             options=options,
         )
-        self.env_type : Environments = env_type
+        self.env_type: Environments = env_type
         self.env = None
         self.objectives_metrics = objectives_metrics
-        self.learning_algo : Algo = learning_algo
+        self.learning_algo: Algo = learning_algo
         self.learning_method = None
         self.logger = getLogger("VIRAL")
-        #self._learning(self.memory[0])
+        # self._learning(self.memory[0])
         self.queue = Queue()
         self.memory: List[State] = [State(0)]
         self.multi_process: list[Process] = []
-        self.multi_process.append(Process(target=self._learning, args=(self.memory[0], self.queue,)))
+        self.multi_process.append(
+            Process(
+                target=self._learning,
+                args=(
+                    self.memory[0],
+                    self.queue,
+                ),
+            )
+        )
         self.multi_process[0].start()
 
     def generate_reward_function(
@@ -80,27 +91,19 @@ class VIRAL:
         # TODO Pourquoi additional_options ici et pas dans le constructeur ?
         additional_options = {
             "temperature": 1,
-            #"num_predict": 3, # l'impression que ça change rien a creuser
-            
-            #"mirostat" : 1,
-            #"mirostat_eta" : 0.01, #gère la vitesse de réponses du model (0.1 par défaut) plus c'est petit plus c'est lent
-            #"mirostat_tau" : 4.0, #gère la balance entre la diversité et la coherence des réponses (5.0 par défaut) plus c'est petit plus c'est focus et cohérent
-            
-            #num_ctx": 2048, # nombre de tokens contextuels (2048 par défaut peut être pas nécessaire de changer)
-            
-            #repeat_last_n": 64, # combien le model regarde en arrière pour éviter de répéter les réponses (64 par défaut large pour nous)
-            
-            #"repeat_penalty": 1.5, # pénalité pour éviter de répéter les réponses (1.1 par défaut au mac 1.5 intéressant a modificer je pense)
-            
-            #"stop": "stop you here" # pour stopper la génération de texte pas intéressant pour nous
-            
-            #"tfs_z": 1.2, #reduire l'impacte des token les moins "pertinents" (1.0 par défaut pour désactiver 2.0 max)
-            
-            #"top_k": 30, #reduit la probabilité de générer des non-sens (40 par défaut, 100 pour générer des réponses plus diverses, 10 pour des réponses plus "conservatrices")
-            #"top_p": 0.95, #marche avec le top_k une forte valeur pour des texte plus diverses (0.9 par défaut)
-            #"min_p": 0.05, #alternative au top_p, vise a s'aéssurer de la balance entre qualité et diversité (0.0 par défaut)
-            
-            #"seed": 42, # a utiliser pour la reproductibilité des résultats (important si publication)
+            # "num_predict": 3, # l'impression que ça change rien a creuser
+            # "mirostat" : 1,
+            # "mirostat_eta" : 0.01, #gère la vitesse de réponses du model (0.1 par défaut) plus c'est petit plus c'est lent
+            # "mirostat_tau" : 4.0, #gère la balance entre la diversité et la coherence des réponses (5.0 par défaut) plus c'est petit plus c'est focus et cohérent
+            # num_ctx": 2048, # nombre de tokens contextuels (2048 par défaut peut être pas nécessaire de changer)
+            # repeat_last_n": 64, # combien le model regarde en arrière pour éviter de répéter les réponses (64 par défaut large pour nous)
+            # "repeat_penalty": 1.5, # pénalité pour éviter de répéter les réponses (1.1 par défaut au mac 1.5 intéressant a modificer je pense)
+            # "stop": "stop you here" # pour stopper la génération de texte pas intéressant pour nous
+            # "tfs_z": 1.2, #reduire l'impacte des token les moins "pertinents" (1.0 par défaut pour désactiver 2.0 max)
+            # "top_k": 30, #reduit la probabilité de générer des non-sens (40 par défaut, 100 pour générer des réponses plus diverses, 10 pour des réponses plus "conservatrices")
+            # "top_p": 0.95, #marche avec le top_k une forte valeur pour des texte plus diverses (0.9 par défaut)
+            # "min_p": 0.05, #alternative au top_p, vise a s'aéssurer de la balance entre qualité et diversité (0.0 par défaut)
+            # "seed": 42, # a utiliser pour la reproductibilité des résultats (important si publication)
         }
         ### INIT STAGE ###
         for i in [1, 2]:
@@ -121,16 +124,24 @@ class VIRAL:
                 float: The reward for the current step
             \"\"\"
         """
-            self.llm.add_message(prompt)
-            response = self.llm.generate_response(
-                stream=True, additional_options=additional_options
-            )
+            # self.llm.add_message(prompt)
+            # response = self.llm.generate_response(
+            #     stream=True, additional_options=additional_options
+            # )
+            response = """
+def reward_func(observations:np.ndarray, terminated: bool, truncated: bool) -> float:
+    _, _, pole_angle, _ = observations
+    
+    if terminated or truncated:
+        return -1.0
+    else:
+        return np.cos(pole_angle)"""
             self.logger.info(f"additional options: {additional_options}")
             response = self.llm.print_Generator_and_return(response, i)
             reward_func, response = self._get_runnable_function(response)
             self.memory.append(State(i, reward_func, response))
-            
-        best_idx, worst_idx = self.evaluate_policy(1, 2) 
+
+        best_idx, worst_idx = self.evaluate_policy(1, 2)
         self.logger.debug(f"state to refine: {worst_idx}")
         ### SECOND STAGE ###
         for n in range(iterations - 1):
@@ -249,19 +260,24 @@ class VIRAL:
 
     def _learning(self, state: State, queue: Queue) -> None:
         """train a policy on an environment"""
-        self.logger.debug(f"state {state.idx} begin is learning with reward function: {state.reward_func_str}")
+        self.logger.debug(
+            f"state {state.idx} begin is learning with reward function: {state.reward_func_str}"
+        )
         vec_env, model = self._generate_env_model(state.reward_func)
         training_callback = TrainingInfoCallback()
-        policy = model.learn(total_timesteps=60000, callback=training_callback)
+        policy = model.learn(total_timesteps=600, callback=training_callback)
+        policy.save(f"model/policy{state.idx}.model")
         metrics = training_callback.get_metrics()
         self.logger.debug(f"TRAINING METRICS: {metrics}")
         sr_test = self.test_policy(vec_env, policy)
         # ajoute au dict metrics les performances sans ecraser les anciennes
         metrics["test_success_rate"] = sr_test
-        self.logger.debug(f"state {state.idx} as finished is learning with performances: {metrics}")
-        queue.put((state.idx, policy, metrics))
+        self.logger.debug(
+            f"state {state.idx} as finished is learning with performances: {metrics}"
+        )
 
-
+        queue.put([state.idx, f"model/policy{state.idx}.model", metrics])
+        self.logger.debug(f"queue size {queue.empty()}")
 
     def evaluate_policy(self, idx1: int, idx2: int) -> int:
         """
@@ -276,23 +292,34 @@ class VIRAL:
         """
         if len(self.memory) < 2:
             self.logger.error("At least two reward functions are required.")
-        to_join: int = []
+        to_get: int = 0
+        to_join: list = []
         for i in [idx1, idx2]:
             if self.memory[i].performances is None:
-                self.multi_process.append(Process(target=self._learning, args=(self.memory[i], self.queue,)))
+                self.multi_process.append(
+                    Process(target=self._learning, args=(self.memory[i], self.queue))
+                )
                 self.multi_process[-1].start()
-                to_join.append(i)
-        # TODO comparaison sur le success rate pour l'instant
+                to_get += 1
+                to_join.append(len(self.multi_process)-1)
+
+        while len(to_get) > 0:
+            try:
+                get = self.queue.get(block=False)
+                self.memory[get[0]].set_policy(get[1])
+                self.memory[get[0]].set_performances(get[2])
+                self.logger.debug(
+                    f"state {get[0]} has finished learning with performances: {get[2]}"
+                )
+                to_get -= 1
+            except Empty:
+                self.logger.debug("Consumer: got nothing, waiting a while...")
+                sleep(1)
+                continue
+
         for p in to_join:
-            self.logger.debug("waiting to join")
+            self.logger.debug(f"waiting to join {p}")
             self.multi_process[p].join()
-                
-        # Retrieve results from the queue
-        while not self.queue.empty():
-            state_idx, policy, metrics = self.queue.get()
-            self.memory[state_idx].set_policy(policy)
-            self.memory[state_idx].set_performances(metrics)
-            self.logger.debug(f"state {state_idx} has finished learning with performances: {metrics}")
         if (
             self.memory[idx1].performances["test_success_rate"]
             > self.memory[idx2].performances["test_success_rate"]
@@ -302,12 +329,7 @@ class VIRAL:
             return idx2, idx1
 
     def test_policy(
-        self,
-        env,
-        policy,
-        reward_func=None,
-        nb_episodes=100,
-        max_t=1000
+        self, env, policy, reward_func=None, nb_episodes=100, max_t=1000
     ) -> list:
         all_rewards = []
         all_states = []
@@ -317,24 +339,28 @@ class VIRAL:
             epi_rewards = 0
             while True:
                 action, _states = policy.predict(obs)
-                obs, reward, dones, info = env.step(action)  # TODO treat multiples envs
+                obs, reward, dones, info = env.step(action)  # TODO treat multiples envs
                 epi_rewards += reward.item()
                 if dones[0]:
                     if info[0]["TimeLimit.truncated"]:
                         nb_success += 1
                     break
             all_rewards.append(epi_rewards)
-        return (nb_success / nb_episodes)
+        return nb_success / nb_episodes
 
     def _generate_env_model(self, reward_func):
         """
         Generate the environment model
         """
-        vec_env = make_vec_env(self.env_type.value, n_envs=1, wrapper_class=CustomRewardWrapper, wrapper_kwargs={'llm_reward_function': reward_func})
+        vec_env = make_vec_env(
+            self.env_type.value,
+            n_envs=1,
+            wrapper_class=CustomRewardWrapper,
+            wrapper_kwargs={"llm_reward_function": reward_func},
+        )
         if self.learning_algo == Algo.PPO:
             model = PPO("MlpPolicy", vec_env, verbose=0, device="cpu")
         else:
             raise ValueError("The learning algorithm is not implemented.")
-        
+
         return vec_env, model
-        
